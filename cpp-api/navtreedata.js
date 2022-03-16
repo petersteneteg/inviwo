fbc66733075122eb1eb83d5a7b464c908a586e0d
Inviwo-Team <team@inviwo.org>
2022-03-16 14:45:34 +0000

    Update C++ API Docs

---
+++ cpp-api/navtreedata.js
@@ -0,0 +1,90 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Inviwo", "index.html", [
+    [ "Inviwo - Interactive Visualization Workshop", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classinviwo_1_1BrushingAndLinkingManager.html#a740c999cb6bcd99eada35131d197e31b",
+"classinviwo_1_1CanvasProcessor.html",
+"classinviwo_1_1CubeProxyGeometry.html",
+"classinviwo_1_1Dialog.html",
+"classinviwo_1_1FontData.html",
+"classinviwo_1_1ImageGLProcessor.html",
+"classinviwo_1_1InviwoApplication.html#a4f61e39351762fff624eab48d1ac5d63",
+"classinviwo_1_1LayerRAM.html",
+"classinviwo_1_1MeshDrawerGL_1_1DrawObject.html#a046702fde8f2ace4d8f46542b0e1204a",
+"classinviwo_1_1Observer.html",
+"classinviwo_1_1PickingEvent.html#afe0ed085636b94a89cfcaa7d13330324",
+"classinviwo_1_1ProcessorNetworkEvaluator.html",
+"classinviwo_1_1PythonScript.html#a77778505292cfa4cdc9663b24dd8de0c",
+"classinviwo_1_1Settings.html#a58e660e86fe12e6c7559f2c613d2e1cd",
+"classinviwo_1_1SpatialCoordinateTransformerImpl.html#ac42eeb093823613a744063c2a65c7746",
+"classinviwo_1_1StructuredCameraCoordinateTransformerImpl.html#a17d2412377cd15ac227d7c617b1965f5",
+"classinviwo_1_1TFPrimitiveSetObservable.html",
+"classinviwo_1_1TouchEvent.html#a3ecd3d197ae74568214a1794b422c1f6",
+"classinviwo_1_1ViewManager.html",
+"classinviwo_1_1WorkspaceManager.html#a545ce3c83eaaa6a9040e3fef57716667",
+"classinviwo_1_1animation_1_1KeyframeSequence.html",
+"classinviwo_1_1glui_1_1Renderer.html#a58299741a4cf2e2b352e6a05ff98b806",
+"classinviwo_1_1transform_1_1TranslateProperty.html#a04a6fbf259f546e4585ce0055338e548",
+"namespaceinviwo_1_1csvfilters.html",
+"namespaceinviwo_1_1xml.html#a7dc7767440cca4416d9da78677e048f2",
+"structinviwo_1_1PortTraits_3_01BaseImageInport_3_011_01_4_01_4.html",
+"structinviwo_1_1detail_1_1PortDeleter.html",
+"structinviwo_1_1util_1_1detail_1_1grow.html",
+"structstd_1_1hash_3_01inviwo_1_1ProcessorPair_01_4.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
